760214906b449e725abf62eb274b47f648106359
Souher Al-Taleb <altalebsouher8@gmail.com>
2026-04-17 23:06:48 +0200

    add nav

--- Portfolio/src/components/layout/MainLayout.tsx
+++ Portfolio/src/components/layout/MainLayout.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 
 import Footer from "./Footer.tsx";
 import Header from "./Header.tsx";
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen bg-soft-gradient">
+    <div className=" min-h-screen  ">
       <Header />
       <main className="max-w-7xl mx-auto px-6 py-12">
         <Outlet />
--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+// import { fileURLToPath } from "node:url";
+
+// const __filename = fileURLToPath(import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()]
+});
